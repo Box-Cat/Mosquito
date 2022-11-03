23d826e1237506db65692eef3ad3f6dfead16791
Box-Cat <pjh211pjh@gmail.com>
2022-11-03 22:56:51 +0900

    22/11/03

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -1,3 +1,12 @@
 import {combineReducers} from 'redux';
 
+const rootReducer = combineReducers({
+   //counter,
+   //todos,
+   // posts
+})
+
+
 export default combineReducers({});
+
+export type RootState = ReturnType<typeof rootReducer>;
